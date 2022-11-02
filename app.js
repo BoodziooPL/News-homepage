fc527c987bd4bc4a0719b5145f0760f4a75212a5
BoodziooPL <bogdanmalik@gmail.com>
2022-11-02 02:30:12 +0100

    add screen

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const hamburger = document.querySelector('.menu')
 const menu = document.querySelector('nav')
 const close = document.querySelector('.closeMenu')
 
-
 const navi = () => {
   menu.classList.add('active')
 }
 const naviClose = () => {
   menu.classList.remove('active')
- 
 }
 
 hamburger.addEventListener('click', navi)
